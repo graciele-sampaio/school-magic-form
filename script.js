0717951fdd2994efe65824e717d6c0c6384a32ce
carolcortes <caroline.ocortes@gmail.com>
2022-05-09 22:05:51 -0300

    alteracoes html e js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const alertLogin = document.querySelectorAll('.btn')[0];
 const email = document.getElementById('email');
 const password = document.getElementById('senha');
 const form = document.getElementById('evaluation-form');
+const submitBtn = document.getElementById('submit-btn');
+const checkAgree = document.getElementById('agreement');
+const counter = document.getElementById('counter');
+const textArea = document.getElementsByTagName('textarea')[0];
 
 // Alert
 function loginTryber() {
@@ -14,22 +18,16 @@ function loginTryber() {
 alertLogin.addEventListener('click', loginTryber);
 
 // Agree and submit
-const submitBtn = document.getElementById('submit-btn');
-const checkAgree = document.getElementById('agreement');
-
 function agree() {
   if (checkAgree.checked) {
     submitBtn.disabled = false;
   } else {
-    submitBtn.disable = true;
+    submitBtn.disabled = true;
   }
 }
 checkAgree.addEventListener('click', agree);
 
 // Counter
-const counter = document.getElementById('counter');
-const textArea = document.getElementsByTagName('textarea')[0];
-
 function maxCount() {
   counter.innerText = 500 - textArea.value.length;
 }
@@ -45,17 +43,36 @@ function getName() {
 }
 
 function getEmail() {
-  
+  const inputEmail = document.createElement('p');
+  inputEmail.innerText = `Email: ${document.getElementById('input-email').value}`;
+  form.appendChild(inputEmail);
 }
 
-submitBtn.addEventListener('click', getName);
+function getHouse() {
+  const house = document.createElement('p');
+  house.innerText = `Casa: ${document.getElementById('house').value}`;
+  form.appendChild(house);
+}
+
+function getFamily() {
+  const family = document.createElement('p');
+  family.innerText = `Família: ${document.querySelector('.family input:checked').value}`;
+  form.appendChild(family);
+}
+
+function sendInfo() {
+  getName();
+  getEmail();
+  getHouse();
+  getFamily();
+}
+submitBtn.addEventListener('click', sendInfo);
 
 function handleSubtmit(event) {
   const myEvent = event;
   myEvent.preventDefault();
 }
 submitBtn.addEventListener('click', handleSubtmit);
-
 
 window.onload = () => {
   maxCount();
